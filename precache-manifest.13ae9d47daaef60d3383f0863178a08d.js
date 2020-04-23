d21b2064a83ee2ccaa869f8b5883eff404b7a56b
tpartridge1 <tammy.partridge7@gmail.com>
2020-04-23 13:16:37 -0400

    Updates

---
+++ precache-manifest.13ae9d47daaef60d3383f0863178a08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d068e5c0502a42202ae061a86d47f95b",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "719e664cd112adf405a4",
+    "url": "/Covid-19-Tracker/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "92fd38d20a03e3ffee3a",
+    "url": "/Covid-19-Tracker/static/css/main.54213975.chunk.css"
+  },
+  {
+    "revision": "719e664cd112adf405a4",
+    "url": "/Covid-19-Tracker/static/js/2.0c4b2ad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-19-Tracker/static/js/2.0c4b2ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92fd38d20a03e3ffee3a",
+    "url": "/Covid-19-Tracker/static/js/main.38573c9b.chunk.js"
+  },
+  {
+    "revision": "6ec4a38b884d86117ba6",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.7d670cf4.js"
+  }
+]);
